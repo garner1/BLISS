68296e09d968e9bb627c4f0fb3d6604ac03f12b9
garner1 <silvano.garnerone@gmail.com>
2018-02-20 14:03:52 +0100

    minor

--- bin/themall.sh
+++ bin/themall.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 exp=L17
 bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
@@ -10,66 +11,6 @@ exp=L19
 bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
 exp=L20
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L21
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L22
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L23
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L24
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L25
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L26
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L27
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L28
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L29
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L30
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L31
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L32
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L33
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L34
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L35
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L36
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L37
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L38
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L39
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
-
-exp=L40
 bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
 exp=L41
@@ -84,29 +25,114 @@ bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zha
 exp=L44
 bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
-exp=L45
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+#########################################################################
+# exp=L17
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
-exp=L46
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+# exp=L18
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
-exp=L47
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+# exp=L19
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
-exp=L48
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+# exp=L20
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
-exp=L49
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+# exp=L21
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
-exp=L50
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+# exp=L22
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
-exp=L51
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+# exp=L23
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
-exp=L52
-bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+# exp=L24
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L25
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L26
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L27
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L28
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L29
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L30
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L31
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L32
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L33
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L34
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L35
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L36
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L37
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L38
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L39
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L40
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L41
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L42
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L43
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L44
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L45
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L46
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L47
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L48
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L49
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L50
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L51
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
+
+# exp=L52
+# bash auxiliary_bliss.sh "$exp" hg19 30 /media/bicroserver-seq/Collaborations/Zhang_lab/bioarchive_submission_bliss_NatComm
 
 ##########################################################################
 
