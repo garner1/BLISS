297442bc22ebfcceafc4651df629fb83dd61fdfb
garner1 <silvano.garnerone@gmail.com>
2018-04-25 14:38:01 +0200

    minor

--- bin/prepare_run.sh
+++ bin/prepare_run.sh
@@ -1,6 +1,6 @@
 #!/bin/usr/env bash
 
-# RUN AS: bash prepare_run.sh samplesheet.csv
+# RUN AS: bash prepare_run.sh samplesheet.csv BICRO66
 # head -1 samplesheet.csv as: BB78b,Caco2_Ctrl_2A,CATCAATC,Homo sapiens,5
 
 inputfile=$1
@@ -14,7 +14,8 @@ echo >> run_pipeline_$run.sh
 
 while read -r line; do
     code=`echo $line|cut -d',' -f1`
-    barcode=`echo $line|cut -d',' -f3`
+    # barcode=`echo $line|cut -d',' -f3`
+    barcode=TGATGATC
     echo bash bliss.sh "$code" human ../patterns/"$code"_*_"$barcode" 30 "$dir" >> run_pipeline_$run.sh
     echo mkdir -p ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> run_pipeline_$run.sh
     echo mv ~/Work/dataset/bliss/"$code"/outdata/* ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> run_pipeline_$run.sh
