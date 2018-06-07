bfddb438cdbe8e0892862efa666532fedfc1a2be
garner1 <silvano.garnerone@gmail.com>
2018-06-07 18:02:48 +0200

    minor

---
+++ bin/run_pipeline_BICRO89.sh
@@ -0,0 +1,98 @@
+#!/bin/usr/env bash
+
+bash bliss.sh BB120 human ../patterns/BB120_K562-DMSO-rp1a_TGATGATC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB120/outdata_TGATGATC
+mv /home/garner1/Work/dataset/bliss/BB120/outdata/* /home/garner1/Work/dataset/bliss/BB120/outdata_TGATGATC
+
+bash bliss.sh BB120 human ../patterns/BB120_Calibrator-20ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB120/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB120/outdata/* /home/garner1/Work/dataset/bliss/BB120/outdata_GTCGTTCC
+
+bash bliss.sh BB121 human ../patterns/BB121_K562-DMSO-rp2a_ACGACATC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB121/outdata_ACGACATC
+mv /home/garner1/Work/dataset/bliss/BB121/outdata/* /home/garner1/Work/dataset/bliss/BB121/outdata_ACGACATC
+
+bash bliss.sh BB121 human ../patterns/BB121_Calibrator-20ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB121/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB121/outdata/* /home/garner1/Work/dataset/bliss/BB121/outdata_GTCGTTCC
+
+bash bliss.sh BB122 human ../patterns/BB122_K562-ETO-rp1a_TGATGATC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB122/outdata_TGATGATC
+mv /home/garner1/Work/dataset/bliss/BB122/outdata/* /home/garner1/Work/dataset/bliss/BB122/outdata_TGATGATC
+
+bash bliss.sh BB122 human ../patterns/BB122_Calibrator-20ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB122/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB122/outdata/* /home/garner1/Work/dataset/bliss/BB122/outdata_GTCGTTCC
+
+bash bliss.sh BB123 human ../patterns/BB123_K562-ETO-rp2a_ACGACATC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB123/outdata_ACGACATC
+mv /home/garner1/Work/dataset/bliss/BB123/outdata/* /home/garner1/Work/dataset/bliss/BB123/outdata_ACGACATC
+
+bash bliss.sh BB123 human ../patterns/BB123_Calibrator-20ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB123/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB123/outdata/* /home/garner1/Work/dataset/bliss/BB123/outdata_GTCGTTCC
+
+bash bliss.sh BB124 human ../patterns/BB124_K562-DMSO-rp1b_TGATGATC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB124/outdata_TGATGATC
+mv /home/garner1/Work/dataset/bliss/BB124/outdata/* /home/garner1/Work/dataset/bliss/BB124/outdata_TGATGATC
+
+bash bliss.sh BB125 human ../patterns/BB125_K562-DMSO-rp2b_ACGACATC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB125/outdata_ACGACATC
+mv /home/garner1/Work/dataset/bliss/BB125/outdata/* /home/garner1/Work/dataset/bliss/BB125/outdata_ACGACATC
+
+bash bliss.sh BB126 human ../patterns/BB126_K562-ETO-rp1b_TGATGATC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB126/outdata_TGATGATC
+mv /home/garner1/Work/dataset/bliss/BB126/outdata/* /home/garner1/Work/dataset/bliss/BB126/outdata_TGATGATC
+
+bash bliss.sh BB127 human ../patterns/BB127_K562-ETO-rp2b_ACGACATC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB127/outdata_ACGACATC
+mv /home/garner1/Work/dataset/bliss/BB127/outdata/* /home/garner1/Work/dataset/bliss/BB127/outdata_ACGACATC
+
+bash bliss.sh BB128 human ../patterns/BB128_OV2295-trp1_CATCACGC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB128/outdata_CATCACGC
+mv /home/garner1/Work/dataset/bliss/BB128/outdata/* /home/garner1/Work/dataset/bliss/BB128/outdata_CATCACGC
+
+bash bliss.sh BB128 human ../patterns/BB128_Calibrator-10ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB128/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB128/outdata/* /home/garner1/Work/dataset/bliss/BB128/outdata_GTCGTTCC
+
+bash bliss.sh BB129 human ../patterns/BB129_OV2295-trp2_GTCGTCGC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB129/outdata_GTCGTCGC
+mv /home/garner1/Work/dataset/bliss/BB129/outdata/* /home/garner1/Work/dataset/bliss/BB129/outdata_GTCGTCGC
+
+bash bliss.sh BB129 human ../patterns/BB129_Calibrator-10ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB129/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB129/outdata/* /home/garner1/Work/dataset/bliss/BB129/outdata_GTCGTTCC
+
+bash bliss.sh BB130 human ../patterns/BB130_OV2295-trp3_ACGACCGC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB130/outdata_ACGACCGC
+mv /home/garner1/Work/dataset/bliss/BB130/outdata/* /home/garner1/Work/dataset/bliss/BB130/outdata_ACGACCGC
+
+bash bliss.sh BB130 human ../patterns/BB130_Calibrator-10ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB130/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB130/outdata/* /home/garner1/Work/dataset/bliss/BB130/outdata_GTCGTTCC
+
+bash bliss.sh BB131 human ../patterns/BB131_RecOV2295-trp1_CATCACGC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB131/outdata_CATCACGC
+mv /home/garner1/Work/dataset/bliss/BB131/outdata/* /home/garner1/Work/dataset/bliss/BB131/outdata_CATCACGC
+
+bash bliss.sh BB131 human ../patterns/BB131_Calibrator-10ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB131/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB131/outdata/* /home/garner1/Work/dataset/bliss/BB131/outdata_GTCGTTCC
+
+bash bliss.sh BB132 human ../patterns/BB132_RecOV2295-trp2_GTCGTCGC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB132/outdata_GTCGTCGC
+mv /home/garner1/Work/dataset/bliss/BB132/outdata/* /home/garner1/Work/dataset/bliss/BB132/outdata_GTCGTCGC
+
+bash bliss.sh BB132 human ../patterns/BB132_Calibrator-10ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB132/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB132/outdata/* /home/garner1/Work/dataset/bliss/BB132/outdata_GTCGTTCC
+
+bash bliss.sh BB133 human ../patterns/BB133_RecOV2295-trp3_ACGACCGC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB133/outdata_ACGACCGC
+mv /home/garner1/Work/dataset/bliss/BB133/outdata/* /home/garner1/Work/dataset/bliss/BB133/outdata_ACGACCGC
+
+bash bliss.sh BB133 human ../patterns/BB133_Calibrator-10ng_GTCGTTCC 30 /media/bicroserver_2-seq/BICRO89/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/BB133/outdata_GTCGTTCC
+mv /home/garner1/Work/dataset/bliss/BB133/outdata/* /home/garner1/Work/dataset/bliss/BB133/outdata_GTCGTTCC
+
