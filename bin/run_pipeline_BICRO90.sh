0e0e5e202329162d5b26f14d6dfd29f16d4f633e
garner1 <silvano.garnerone@gmail.com>
2018-06-21 10:49:26 +0200

    minor

---
+++ bin/run_pipeline_BICRO90.sh
@@ -0,0 +1,26 @@
+#!/bin/usr/env bash
+
+bash bliss.sh RM138 human ../patterns/RM138_x_GTCGTATC 30 /media/bicroserver_2-seq/BICRO90/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/RM138/outdata_GTCGTATC
+mv /home/garner1/Work/dataset/bliss/RM138/outdata/* /home/garner1/Work/dataset/bliss/RM138/outdata_GTCGTATC
+
+bash bliss.sh RM139 human ../patterns/RM139_x_CATCAATC 30 /media/bicroserver_2-seq/BICRO90/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/RM139/outdata_CATCAATC
+mv /home/garner1/Work/dataset/bliss/RM139/outdata/* /home/garner1/Work/dataset/bliss/RM139/outdata_CATCAATC
+
+bash bliss.sh RM140 human ../patterns/RM140_x_CATCAATC 30 /media/bicroserver_2-seq/BICRO90/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/RM140/outdata_CATCAATC
+mv /home/garner1/Work/dataset/bliss/RM140/outdata/* /home/garner1/Work/dataset/bliss/RM140/outdata_CATCAATC
+
+bash bliss.sh RM141 human ../patterns/RM141_x_CATCAATC 30 /media/bicroserver_2-seq/BICRO90/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/RM141/outdata_CATCAATC
+mv /home/garner1/Work/dataset/bliss/RM141/outdata/* /home/garner1/Work/dataset/bliss/RM141/outdata_CATCAATC
+
+bash bliss.sh RM142 human ../patterns/RM142_x_CATCAATC 30 /media/bicroserver_2-seq/BICRO90/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/RM142/outdata_CATCAATC
+mv /home/garner1/Work/dataset/bliss/RM142/outdata/* /home/garner1/Work/dataset/bliss/RM142/outdata_CATCAATC
+
+bash bliss.sh RM143 human ../patterns/RM143_x_CATCAATC 30 /media/bicroserver_2-seq/BICRO90/FASTQ
+mkdir -p /home/garner1/Work/dataset/bliss/RM143/outdata_CATCAATC
+mv /home/garner1/Work/dataset/bliss/RM143/outdata/* /home/garner1/Work/dataset/bliss/RM143/outdata_CATCAATC
+
