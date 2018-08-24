dfa2241244e653cbc059a138cbd4c8279577751f
garner1 <silvano.garnerone@gmail.com>
2018-08-24 10:05:44 +0200

    minor

---
+++ bin/runs/run_pipeline_X.sh
@@ -0,0 +1,30 @@
+#!/bin/usr/env bash
+
+bash bliss.sh RM136 human ../patterns/RM136_Caov3-1-240ng-bulk-rep1_TGATGATC 30 /home/garner1/Work/dataset/bliss
+mkdir -p /home/garner1/Work/dataset/bliss/RM136/outdata_TGATGATC
+mv /home/garner1/Work/dataset/bliss/RM136/outdata/* /home/garner1/Work/dataset/bliss/RM136/outdata_TGATGATC
+
+bash bliss.sh RM137 human ../patterns/RM137_Caov3-2-240ng-bulk-rep2_TGATGATC 30 /home/garner1/Work/dataset/bliss
+mkdir -p /home/garner1/Work/dataset/bliss/RM137/outdata_TGATGATC
+mv /home/garner1/Work/dataset/bliss/RM137/outdata/* /home/garner1/Work/dataset/bliss/RM137/outdata_TGATGATC
+
+bash bliss.sh RM135 human ../patterns/RM135_IMR90-1-292ng-bulk-rep1_TGATGATC 30 /home/garner1/Work/dataset/bliss
+mkdir -p /home/garner1/Work/dataset/bliss/RM135/outdata_TGATGATC
+mv /home/garner1/Work/dataset/bliss/RM135/outdata/* /home/garner1/Work/dataset/bliss/RM135/outdata_TGATGATC
+
+bash bliss.sh RM138 human ../patterns/RM138_IMR90-2-292ng-bulk-rep2_GTCGTATC 30 /home/garner1/Work/dataset/bliss
+mkdir -p /home/garner1/Work/dataset/bliss/RM138/outdata_GTCGTATC
+mv /home/garner1/Work/dataset/bliss/RM138/outdata/* /home/garner1/Work/dataset/bliss/RM138/outdata_GTCGTATC
+
+bash bliss.sh RM139 human ../patterns/RM139_HelaS3-1-600ng-bulk-rep1_CATCAATC 30 /home/garner1/Work/dataset/bliss
+mkdir -p /home/garner1/Work/dataset/bliss/RM139/outdata_CATCAATC
+mv /home/garner1/Work/dataset/bliss/RM139/outdata/* /home/garner1/Work/dataset/bliss/RM139/outdata_CATCAATC
+
+bash bliss.sh RM140 human ../patterns/RM140_HelaS3-2-600ng-bulk-rep2_CATCAATC 30 /home/garner1/Work/dataset/bliss
+mkdir -p /home/garner1/Work/dataset/bliss/RM140/outdata_CATCAATC
+mv /home/garner1/Work/dataset/bliss/RM140/outdata/* /home/garner1/Work/dataset/bliss/RM140/outdata_CATCAATC
+
+bash bliss.sh RM141 human ../patterns/RM141_Caov3-1-179ng-G1-sorted-rep1_CATCAATC 30 /home/garner1/Work/dataset/bliss
+mkdir -p /home/garner1/Work/dataset/bliss/RM141/outdata_CATCAATC
+mv /home/garner1/Work/dataset/bliss/RM141/outdata/* /home/garner1/Work/dataset/bliss/RM141/outdata_CATCAATC
+
