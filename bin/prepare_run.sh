5582bbf9a59ec0b59c8f66bd7f54b82ca8b623ab
garner1 <silvano.garnerone@gmail.com>
2018-10-16 08:23:46 +0200

    minor

--- bin/prepare_run.sh
+++ bin/prepare_run.sh
@@ -4,9 +4,10 @@
 # head -1 samplesheet.csv as: BB78b,Caco2_Ctrl_2A,CATCAATC,Homo sapiens,5
 
 inputfile=$1
-dir=/home/garner1/Work/dataset/bliss
-# dir=/media/bicroserver_2-seq/$run/FASTQ
-run=X
+run=$2
+
+# dir=/home/garner1/Work/dataset/bliss
+dir=/media/bicroserver_2-seq/$run/FASTQ
 rm -f run_pipeline_$run.sh
 
 echo "#!/bin/usr/env bash" >> run_pipeline_$run.sh
@@ -14,6 +15,7 @@ echo >> run_pipeline_$run.sh
 
 while read -r line; do
     code=`echo $line|cut -d',' -f1`
+    sample=`echo $line|cut -d',' -f2`
     barcode=`echo $line|cut -d',' -f3`
     echo bash bliss.sh "$code" human ../patterns/"$code"_*_"$barcode" 30 "$dir" >> run_pipeline_$run.sh
     echo mkdir -p ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> run_pipeline_$run.sh
