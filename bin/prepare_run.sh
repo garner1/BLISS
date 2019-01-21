a02fe23258c0ba27aa115ac820b34a8310b5d2d3
garner1 <silvano.garnerone@gmail.com>
2019-01-21 12:52:54 +0100

    BICRO144

--- bin/prepare_run.sh
+++ bin/prepare_run.sh
@@ -8,18 +8,18 @@ run=$2
 
 # dir=/home/garner1/Work/dataset/bliss
 dir=/media/bicroserver_2-seq/$run/FASTQ
-rm -f run_pipeline_$run.sh
+rm -f ./runs/run_pipeline_$run.sh
 
-echo "#!/bin/usr/env bash" >> run_pipeline_$run.sh
-echo >> run_pipeline_$run.sh
+echo "#!/bin/usr/env bash" >> ./runs/run_pipeline_$run.sh
+echo >> ./runs/run_pipeline_$run.sh
 
 while read -r line; do
     code=`echo $line|cut -d',' -f1`
     sample=`echo $line|cut -d',' -f2`
     barcode=`echo $line|cut -d',' -f3`
-    echo bash bliss.sh "$code" human ~/Work/pipelines/BLISS/patterns/"$code"_*_"$barcode" 30 "$dir" >> run_pipeline_$run.sh
-    echo mkdir -p ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> run_pipeline_$run.sh
-    echo mv ~/Work/dataset/bliss/"$code"/outdata/* ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> run_pipeline_$run.sh
-    echo >> run_pipeline_$run.sh
+    echo bash ~/Work/pipelines/BLISS/bin/bliss.sh "$code" human ~/Work/pipelines/BLISS/patterns/"$code"_*_"$barcode" 30 "$dir" >> ./runs/run_pipeline_$run.sh
+    echo mkdir -p ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> ./runs/run_pipeline_$run.sh
+    echo mv ~/Work/dataset/bliss/"$code"/outdata/* ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> ./runs/run_pipeline_$run.sh
+    echo >> ./runs/run_pipeline_$run.sh
 done < $inputfile
-chmod +x run_pipeline_$run.sh
+chmod +x ./runs/run_pipeline_$run.sh
