6ea1806aab814b1605d3e639c94fd701c236ebe7
garner1 <silvano.garnerone@gmail.com>
2019-04-11 15:05:35 +0200

    minor

--- bin/prepare_run.sh
+++ bin/prepare_run.sh
@@ -3,11 +3,12 @@
 # RUN AS: bash prepare_run.sh samplesheet.csv BICRO66
 # head -1 samplesheet.csv as: BB78b,Caco2_Ctrl_2A,CATCAATC,Homo sapiens,5
 
-inputfile=$1
+inputfile=$1 #samplesheet.csv
 run=$2
+dir=$3 # fullpath to dir containing FASTQ files
+#dir=/home/garner1/Work/dataset/FASTQ
+#dir=/media/bicroserver_2-seq/$run/FASTQ
 
-# dir=/home/garner1/Work/dataset/bliss
-dir=/media/bicroserver_2-seq/$run/FASTQ
 rm -f ./runs/run_pipeline_$run.sh
 
 echo "#!/bin/usr/env bash" >> ./runs/run_pipeline_$run.sh
@@ -19,7 +20,9 @@ while read -r line; do
     barcode=`echo $line|cut -d',' -f3`
     echo bash ~/Work/pipelines/BLISS/bin/bliss.sh "$code" human ~/Work/pipelines/BLISS/patterns/"$code"_*_"$barcode" 30 "$dir" >> ./runs/run_pipeline_$run.sh
     echo mkdir -p ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> ./runs/run_pipeline_$run.sh
+    echo mkdir -p ~/Work/dataset/bliss/"$code"/auxdata_"$barcode" >> ./runs/run_pipeline_$run.sh
     echo mv ~/Work/dataset/bliss/"$code"/outdata/* ~/Work/dataset/bliss/"$code"/outdata_"$barcode" >> ./runs/run_pipeline_$run.sh
+    echo mv ~/Work/dataset/bliss/"$code"/auxdata/* ~/Work/dataset/bliss/"$code"/auxdata_"$barcode" >> ./runs/run_pipeline_$run.sh
     echo >> ./runs/run_pipeline_$run.sh
 done < $inputfile
 chmod +x ./runs/run_pipeline_$run.sh
